28753b3879a4818c4144350bd50832cf80b01e76
yicheng <2816146402@qq.com>
2026-01-15 20:30:11 +0800

    苍穹外卖初始代码

--- sky-take-out/sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-take-out/sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -1,6 +1,7 @@
 package com.sky.interceptor;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.context.BaseContext;
 import com.sky.properties.JwtProperties;
 import com.sky.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
@@ -45,8 +46,9 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
         try {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
-            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
+            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());//获取员工id
             log.info("当前员工id：", empId);
+            BaseContext.setCurrentId(empId);
             //3、通过，放行
             return true;
         } catch (Exception ex) {
